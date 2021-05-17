b41a8b9242e51908ae4b0e7c8c5491698d1867ec
Olesia <odobrogor@gmail.com>
2021-05-17 18:28:40 +0300

    StartEngine and Rate Buttons

--- MyCars/ViewController.swift
+++ MyCars/ViewController.swift
@@ -3,13 +3,14 @@
 //  MyCars
 //
 //  Created by Ivan Akulov on 08/02/20.
-//  Copyright © 2020 Ivan Akulov. All rights reserved.
 //
 
 import UIKit
 import CoreData
 
 class ViewController: UIViewController {
+    
+    var car: Car!
     
     lazy var dateFormatter: DateFormatter = {
         let df = DateFormatter()
@@ -34,11 +35,44 @@ class ViewController: UIViewController {
     }
     
     @IBAction func startEnginePressed(_ sender: UIButton) {
-        
+        car.timesDriven += 1
+        car.lastStarted = Date()
+        do {
+            try context.save()
+            insertDataFrom(selectedCar: car)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
     }
     
     @IBAction func rateItPressed(_ sender: UIButton) {
-        
+        let alertController = UIAlertController(title: "Rate it", message: "Rate this car, please", preferredStyle: .alert)
+        let rateAction = UIAlertAction(title: "Rate", style: .default) { action in
+            if let text = alertController.textFields?.first?.text {
+                self.update(rating: (text as NSString).doubleValue)
+            }
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default)
+        alertController.addTextField { textField in
+            textField.keyboardType = .numberPad
+        }
+        alertController.addAction(rateAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    private func update(rating: Double){
+        car.rating = rating
+        do{
+            try context.save()
+            insertDataFrom(selectedCar: car)
+        } catch let error as NSError {
+            let alertController = UIAlertController(title: "Wrong value", message: "Wrong input data", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default)
+            alertController.addAction(okAction)
+            present(alertController, animated: true)
+            print(error.localizedDescription)
+        }
     }
     
     private func insertDataFrom(selectedCar car: Car){
@@ -117,7 +151,7 @@ class ViewController: UIViewController {
         
         do{
             let results = try context.fetch(fetchRequest)
-            let car = results.first
+            car = results.first
             insertDataFrom(selectedCar: car!)
         } catch let error as NSError {
             print(error.localizedDescription)
